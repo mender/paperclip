0397428ddfcbc79c1a5a7ab09416fa3862c922d0
Горбов Алексей <gorbov@ek.apress.ru>
2011-10-17 16:02:45 +0600

    except_formats support

--- lib/paperclip/interpolations.rb
+++ lib/paperclip/interpolations.rb
@@ -93,7 +93,9 @@ module Paperclip
     # If the style has a format defined, it will return the format instead
     # of the actual extension.
     def extension attachment, style_name
-      return File.extname(attachment.original_filename).gsub(/^\.+/, "") if attachment.instance.created_at <= Time.utc(2011, 10, 17, 8, 30, 0)
+      if !attachment.instance.respond_to?(:created_at) || attachment.instance.created_at <= Time.utc(2011, 10, 17, 8, 30, 0)
+        return File.extname(attachment.original_filename).gsub(/^\.+/, "")
+      end
 
       style = attachment.styles[style_name]
       ext   = (style && style[:format]) || File.extname(attachment.original_filename).gsub(/^\.+/, "")
